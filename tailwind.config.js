cfae8339095cb5629c18dcec5e654f115dda821a
Shivam Chandra <chandrshivam@gmail.com>
2022-07-18 11:54:04 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  // mode: "jit",
+  // // These paths are just examples, customize them to match your project structure
+  // purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
